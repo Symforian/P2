6bdca7afae72a2f7e680c05f570629256878c832
Adrian <adrienkaminski@wp.pl>
2017-11-28 04:37:12 +0100

    Added kucharze. Probably working.

--- osadnicy.cpp
+++ osadnicy.cpp
@@ -12,15 +12,15 @@ int kostka()
 	return rand()%6+1;
 }
 int mysliwi, kucharze, zwierzyna, pozywienie;
-//int sleeping=0;
 int blocked=0;
 int today = 0;
 pthread_mutex_t zwierzynaLock = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t pozywienieLock = PTHREAD_MUTEX_INITIALIZER; ;//, 
+pthread_mutex_t pozywienieLock = PTHREAD_MUTEX_INITIALIZER; ;
 pthread_mutex_t BlockLock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t warunekCzekania = PTHREAD_COND_INITIALIZER;
 void *poluj(void*)
 {
-	//bool sleep = false;
+
 	for(int i =0; i<Czas; i++)
 	{
 	cout<<"Myśliwy żyje dzień "<<i<<endl;
@@ -33,9 +33,6 @@ void *poluj(void*)
 		{
 		zwierzyna--;
 		pthread_mutex_unlock(&zwierzynaLock);
-		pthread_mutex_lock(&pozywienieLock);
-		pozywienie++;
-		pthread_mutex_unlock(&pozywienieLock);
 		cout<<"\nMyśliwy:UPOLOWAŁ.\n";
 		}
 	else
@@ -55,37 +52,90 @@ void *poluj(void*)
 	mysliwi--;
 	break;
 	}
-	int wut;
 
-	cout<<"\nMyśliwy:ZzZzZz.\n";//<<wut<<endl;
-	//while(sem_wait(&Time)!=-1)
-	//if(sem_wait(&Time)==-1 && errno == EINTR)
-	//while(
-	//while(blocked==-1)
-	//usleep(1);
+
+	cout<<"\nMyśliwy:ZzZzZz.\n";
+
 	blocked++;
-	//for(int i=0; i<mysliwi;i++)
-	//sem_getvalue(&Time,&wut);
-	//cout<<wut<<endl;
-	if(sem_wait(&Time)==-1)
-	perror("Error sem wait");
-	
-	while(true)
-	{
-	//cout<<"zzzz"<<endl;
-	if(today>i)
-	break;
-	}
 
-	//else 
-	//	break;
-	//sem_getvalue(&Time,&wut);
-	cout<<"\nMyśliwy wstał.\n";//<<wut<<endl;
+	//if(sem_wait(&Time)==-1)
+	//perror("Error sem wait myśliwy");
+		
+	pthread_mutex_lock(&BlockLock);
+	while(i==today)
+	{
+	pthread_cond_wait(&warunekCzekania,&BlockLock);
+//	cout<<"mys"<<endl;
+	//if(today>i)
+	//break;
+	}
+	pthread_mutex_unlock(&BlockLock);
+
+
+	cout<<"\nMyśliwy wstał.\n";
 	}
 
 	return NULL;
 }
+void *gotuj(void*)
+{
 
+	for(int i =0; i<Czas; i++)
+	{
+	cout<<"Kucharz żyje dzień "<<i<<endl;
+	
+	int talent = kostka();
+	pthread_mutex_lock(&zwierzynaLock);
+	
+
+	if(zwierzyna>0)
+	{	zwierzyna--;
+		pthread_mutex_unlock(&zwierzynaLock);
+		cout<<"\nKucharz:Ugotował:"<<talent<<"\n";
+	}
+	else
+	pthread_mutex_unlock(&zwierzynaLock);
+	
+	pozywienie+=talent;
+	if(pozywienie>0)
+	{
+	pozywienie--;
+	pthread_mutex_unlock(&pozywienieLock);
+	cout<<"Kucharz:ZJADŁ."<<endl;
+	}
+	else
+	{
+	pthread_mutex_unlock(&pozywienieLock);
+	cout<<"\nKucharz:DEAD.\n";
+	kucharze--;
+	break;
+	}
+
+
+
+	cout<<"\nKucharz:ZzZzZz.\n";
+
+	blocked++;
+
+	//if(sem_wait(&Time)==-1)
+	//perror("Error sem wait kucharz");
+	
+	pthread_mutex_lock(&BlockLock);
+	while(i==today)
+	{
+	pthread_cond_wait(&warunekCzekania,&BlockLock);
+//	cout<<"mys"<<endl;
+	//if(today>i)
+	//break;
+	}
+	pthread_mutex_unlock(&BlockLock);
+
+
+	cout<<"\nKucharz wstał.\n";
+	}
+
+	return NULL;
+}
 
 int main( int argc, char * argv[] )
 {
@@ -98,121 +148,78 @@ int main( int argc, char * argv[] )
 	kucharze = 11;
 	zwierzyna = 3;
 	pozywienie = 7;
+cout<<"Hello "<<argc<<endl;
+	if(argc>1)
+	mysliwi = atoi(argv[1]);
+	if(argc>2)
+	kucharze = atoi(argv[2]);//cout<<"seg"<<endl;}
+	if(argc>3)
+	zwierzyna = atoi(argv[3]);
+	if(argc>4)
+	pozywienie = atoi(argv[4]);
 
-	if(argc!=1)
-	{
-		 for(;argc>0;argc--)
-		{
-			if(argc==1)
-			mysliwi = atoi(argv[argc]);
-			else if(argc==2)
-			kucharze = atoi(argv[argc]);
-			else if(argc==3)
-			zwierzyna = atoi(argv[argc]);
-			else if(argc==4)
-			pozywienie = atoi(argv[argc]);
 
-		}
-	}
-	/*if (pthread_mutex_init(&zwierzynaLock,NULL) != 0)
-	{
-		printf("\n zwierzyna mutex init failed\n");
-		return 1;
-	}
-	if (pthread_mutex_init(&pozywienieLock,NULL) != 0)
-	{
-		printf("\n pozywienie mutex init failed\n");
-		return 1;
-	}*/
-	//if (pthread_mutex_init(&BlockLock,NULL) != 0)
-	//{
-	//	printf("\n BlockLock mutex init failed\n");
-	//	return 1;
-	//}
 	cout<<"Myśliwych jest "<<mysliwi<<"\n Kucharzy jest "<<kucharze<<"\n Zwierzyny jest "<<zwierzyna<<"\n Pożywienia jest "<<pozywienie<<endl;
 	int byloM = mysliwi;
-	//int byloK = kucharze; 
+	int byloK = kucharze; 
 	pthread_t pthread_mysliwi[mysliwi];
-	//cout<<sleeping<<"<"<<endl;
+	pthread_t pthread_kucharze[mysliwi];
+
 	if(sem_init(&Time, 0, 0)==-1)
 	{
 		printf("\n Semafor init error\n");
 		return 1;
 	}
-	//cout<<sleeping<<"<"<<endl;
-	//if(sem_open("Time",5)==SEM_FAILED)
-	//{
-	//	cout<<"\n Semafor init error\n"<<sem_open("Time",O_CREAT);
-	//	return 1;
-	//}
-	//else
-	//Time = *sem_open("Times",O_CREAT);
-	
 
-	//sem_getvalue(&Time,&sleeping);
-	//cout<<sleeping<<endl;
+
 	for(int i=0; i<mysliwi; i++)
 	{
 		pthread_create(&pthread_mysliwi[i],NULL,&poluj,NULL);
 	}
-
-	while(mysliwi!=0)
+	for(int i=0; i<kucharze; i++)
 	{
-		//sem_getvalue(&Time,&sleeping);
-		if(blocked==mysliwi)//nowy dzien
+		pthread_create(&pthread_kucharze[i],NULL,&gotuj,NULL);
+	}
+	//int tick = 0;
+	while(mysliwi+kucharze!=0 && today<Czas)
+	{
+
+		if(blocked==mysliwi+kucharze)//nowy dzien
 		{
 
-
+pthread_mutex_lock(&BlockLock);
 			cout<<"---Dzień "<<today+1<<"---"<<endl;
 			cout<<"Pożywienie: "<<pozywienie<<"\tZwierzyna: "<<zwierzyna<<endl;
-			//pthread_mutex_lock(&BlockLock);
-			
-			
-			for(int i = 0; i<=blocked; i++)
-				{if(sem_post(&Time)==-1)
-perror("Error sem post");}
-//sem_getvalue(&Time,&sleeping);
-//cout<<"WSTAWAC! "<<sleeping<<endl;}
-			//pthread_mutex_unlock(&BlockLock);
-			today++;
+			cout<<"Myśliwi: "<<mysliwi<<"\tKucharze: "<<kucharze<<endl;
 			blocked=0;
+			pthread_cond_broadcast(&warunekCzekania);
+			//for(int i = 0; i<=blocked; i++)
+			//	{
+			//	if(sem_post(&Time)==-1)
+			//	perror("Error sem post");
+			//	}
+
+			today++;
+
+pthread_mutex_unlock(&BlockLock);
 		}
-	//cout<<blocked<<endl;
-
-
-
+	//	if(tick%200==0)
+		//cout<<blocked<<"\t"<<mysliwi<<"\t"<<kucharze<<endl;
+	//	tick++;
 
 	}
-	/*for(int i =0; i<Czas; i++)
-	{
-	//if(sleeping+mysliwi==-1)
-	//for(int i=0; i<mysliwi;)
-	//{
-	//	sem_post(&Time);
-	//}
-	sem_getvalue(&Time,&sleeping);
-	cout<<"Dzień "<<i+1<<"\n Myśliwych jest "<<mysliwi<<"\n Kucharzy jest "<<kucharze<<"\n Zwierzyny jest "<<zwierzyna<<"\n Pożywienia jest "<<pozywienie<<endl<<sleeping<<endl;
 	
-	if(mysliwi == 0)
-	{
-	cout<<"\nWszyscy myśliwi zmarli\n";
-	break;
-	}
-	sem_getvalue(&Time,&sleeping);
-	//while(sleeping+mysliwi<-1)
-	//{
-	//	sem_getvalue(&Time,&sleeping);
-	//}
-
-	}*/
 
 	for(int i=0; i<byloM; i++)
 	{
 		pthread_join(pthread_mysliwi[i],NULL);
 	}
+	for(int i=0; i<byloK; i++)
+	{
+		pthread_join(pthread_kucharze[i],NULL);
+	}
 	pthread_mutex_destroy(&zwierzynaLock);
 	pthread_mutex_destroy(&pozywienieLock);
-	//pthread_mutex_destroy(&BlockLock);
 	sem_destroy(&Time);
 	return 0;
 }
